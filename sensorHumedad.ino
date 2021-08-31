df4ef0909cce1bc44d4e26a10d1699bf9dbfcc1b
Alfredo Sierra <52094380+llFREEDll@users.noreply.github.com>
2021-08-31 18:55:16 -0500

    Update sensorHumedad.ino

--- sensorHumedad.ino
+++ sensorHumedad.ino
@@ -1,14 +1,18 @@
 void setup() {
+  //pin para encender el motor
   pinMode(12,OUTPUT);  
 
 }
 
 void loop() {
     digitalWrite(12,LOW);
- int readA=analogRead(A0);
- int  show=map(readA,1023,0,0,100);
-  Serial.println(show);
+    // lee lo que detecta el sensor
+   int readA=analogRead(A0);
+   //mapea los datos en una escala del 0 al 100
+   int  show=map(readA,1023,0,0,100);
+   Serial.println(show);
+  //si la humedad es menor al 50% se enciende la bomba de agua  
   if (show<50)
-    digitalWrite(12,HIGH);
+      digitalWrite(12,HIGH);
   delay(1000);
 }
